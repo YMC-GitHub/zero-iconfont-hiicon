d42b33743f7767036fc9777bd7bd62db9114c604
yemiancheng <ymc-github@gmail.com>
2023-10-24 12:45:09 +0800

    build(core): put font files here for sharing

---
+++ fonts/iconfont.d.ts
@@ -0,0 +1,9 @@
+export enum Iconfont {
+  Home = "iconfont-home",
+  Lock = "iconfont-lock",
+  Setting = "iconfont-setting"
+}
+
+export type IconfontClassname = "iconfont-home" | "iconfont-lock" | "iconfont-setting"
+export type IconfontIcon = "home" | "lock" | "setting"
+export const IconfontPrefix = "iconfont-"
