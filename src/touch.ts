5e9b5c47d60b926ed220ad94d3a3360200d41129
yemiancheng <ymc-github@gmail.com>
2023-10-25 14:40:34 +0800

    build(core): add script to touch file in nodejs

---
+++ src/touch.ts
@@ -0,0 +1,24 @@
+// touch -- mock bash cmd touch
+
+import { readFileSync, writeFileSync, readdirSync, existsSync, statSync,mkdirSync } from 'fs'
+import { dirname,resolve,join} from 'path'
+const {log}=console
+
+function saveTextFile(loc:string,text=''){
+    makedirs(loc)
+    log(`[info] out: ${loc}`)
+    writeFileSync(loc,text)
+}
+function makedirs(loc:string){
+    let dir = dirname(resolve(loc))
+    if(!existsSync(dir)) mkdirSync(dir,{recursive:true})
+}
+
+export function touch(loc:string){
+    // do not anything when exist loc
+    if(existsSync(loc)) return loc
+
+    makedirs(loc)
+    // add an empty text file
+    saveTextFile(loc)
+}
