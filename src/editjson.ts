c4bda96f764b53252b7c6c21425276974a3c85bd
yemiancheng <ymc-github@gmail.com>
2023-11-04 19:50:55 +0800

    style(core): auto fomat when save

--- src/editjson.ts
+++ src/editjson.ts
@@ -1,10 +1,10 @@
 
 
-import { writeFileSync,existsSync ,mkdirSync,readFileSync} from 'fs'
-import { dirname,resolve} from 'path'
+import { writeFileSync, existsSync, mkdirSync, readFileSync } from 'fs'
+import { dirname, resolve } from 'path'
 
-function noop() {}
-export function writeJsonFileSync(loc:string, data:any) {
+function noop() { }
+export function writeJsonFileSync(loc: string, data: any) {
     try {
         let tmp = data
         if (typeof data !== 'string') {
@@ -18,22 +18,24 @@ export function writeJsonFileSync(loc:string, data:any) {
 
 // export util for sharing to other files or package
 // when they are too much, move them to anoter file or package
-export function writeTextFileSync(loc:string,text=''){
+export function writeTextFileSync(loc: string, text = '') {
     makedirs(loc)
     // log(`[info] out: ${loc}`)
-    writeFileSync(loc,text)
+    writeFileSync(loc, text)
 }
-export function makedirs(loc:string){
+export function makedirs(loc: string) {
     let dir = dirname(resolve(loc))
-    if(!existsSync(dir)) mkdirSync(dir,{recursive:true})
+    if (!existsSync(dir)) mkdirSync(dir, { recursive: true })
 }
 
 
-export function readJsonFileSync(loc:string,defaultText='{}'){
-    return JSON.parse(readTextFileSync(loc,defaultText))
+export function readJsonFileSync(loc: string, defaultText = '{}') {
+    return JSON.parse(readTextFileSync(loc, defaultText))
+
+
 }
 
-export function readTextFileSync(loc:string,defaultText=''){
+export function readTextFileSync(loc: string, defaultText = '') {
     let text = ''
     try {
         text = readFileSync(loc).toString()
@@ -44,35 +46,35 @@ export function readTextFileSync(loc:string,defaultText=''){
 }
 
 // editjson-transform-sortkeys
-export function sortJsonByKeys(json:any,keys:string){
+export function sortJsonByKeys(json: any, keys: string) {
     // str-to-arr,trim,no-empty
     let arr = keys.split(',')
-    arr=arr.map(v=>v.trim()).filter(v=>v)
+    arr = arr.map(v => v.trim()).filter(v => v)
 
-    let otherkeys = Object.keys(json).filter(a=>!arr.includes(a))
-    let res:any={}
-    res=assignValueByKey(res,arr,json)
-    res=assignValueByKey(res,otherkeys,json)
+    let otherkeys = Object.keys(json).filter(a => !arr.includes(a))
+    let res: any = {}
+    res = assignValueByKey(res, arr, json)
+    res = assignValueByKey(res, otherkeys, json)
     return res
 }
-function assignValueByKey(res:any,arr:string[],json:any){
+function assignValueByKey(res: any, arr: string[], json: any) {
     for (let index = 0; index < arr.length; index++) {
         let key = arr[index]
-        res[key]=json[key]  
+        res[key] = json[key]
     }
-    return res     
+    return res
 }
 
 // editjson-plugin-pickkeys
-export function selectValueByKeys(json:any,keys:string){
+export function selectValueByKeys(json: any, keys: string) {
     // str-to-arr,trim,no-empty
     let arr = keys.split(',')
-    arr=arr.map(v=>v.trim()).filter(v=>v)
+    arr = arr.map(v => v.trim()).filter(v => v)
 
-    let res:any={}
+    let res: any = {}
     for (let index = 0; index < arr.length; index++) {
         let key = arr[index]
-        res[key]=json[key]  
+        res[key] = json[key]
     }
     return res
 }
@@ -80,102 +82,102 @@ export function selectValueByKeys(json:any,keys:string){
 
 // editjson-transform-keywrods
 export interface EditKeywordOption {
-    include:string,
-    exclude:string,
-    sep:string,
-    ns:string
+    include: string,
+    exclude: string,
+    sep: string,
+    ns: string
 }
 export type EditKeywordOptionLike = Partial<EditKeywordOption>
-const builtinEditKeywordOption = {include:'',exclude:'',sep:',',ns:'keywords'} 
-export function editKeywords(data:any,opts?:EditKeywordOptionLike){
-    let buitinOption:EditKeywordOption = builtinEditKeywordOption
-    let option =opts? {...buitinOption,...opts}:buitinOption
-    let {include,exclude,sep,ns}=option
-    let dataInNs=data[ns]
-    let res:string[]=dataInNs?dataInNs:[]
-    res=kwInclude(res,kwArrify(include,sep))
-    res=kwExclude(res,kwArrify(exclude,sep))
-    res=kwDup(res)
+const builtinEditKeywordOption = { include: '', exclude: '', sep: ',', ns: 'keywords' }
+export function editKeywords(data: any, opts?: EditKeywordOptionLike) {
+    let buitinOption: EditKeywordOption = builtinEditKeywordOption
+    let option = opts ? { ...buitinOption, ...opts } : buitinOption
+    let { include, exclude, sep, ns } = option
+    let dataInNs = data[ns]
+    let res: string[] = dataInNs ? dataInNs : []
+    res = kwInclude(res, kwArrify(include, sep))
+    res = kwExclude(res, kwArrify(exclude, sep))
+    res = kwDup(res)
     // update to data
-    data[ns]=res
+    data[ns] = res
     // return {keywords:res,json:data}
     return res
 }
 
-function kwArrify(s:string,sep:string=','){
+function kwArrify(s: string, sep: string = ',') {
     return s.split(sep)
 }
-function kwInclude(cur:string[],toadd:string[]){
-    return [...cur,...toadd]
+function kwInclude(cur: string[], toadd: string[]) {
+    return [...cur, ...toadd]
 }
-function kwExclude(cur:string[],todel:string[]){
-    return cur.filter(v=>!todel.includes(v))
+function kwExclude(cur: string[], todel: string[]) {
+    return cur.filter(v => !todel.includes(v))
 }
-function kwDup(cur:string[]){
+function kwDup(cur: string[]) {
     return Array.from(new Set([...cur]))
 }
 
 // editjson-transform-name
 export interface EditNameOption {
-    ns:string
-    org:string,
-    name:string,
+    ns: string
+    org: string,
+    name: string,
 }
 export type EditNameOptionLike = Partial<EditNameOption>
-const builtinEditNameOption= {org:'',name:'',ns:'name'} 
+const builtinEditNameOption = { org: '', name: '', ns: 'name' }
 
-export function editName(data:any,opts?:EditNameOptionLike){
-    let buitinOption:EditNameOption = builtinEditNameOption
-    let option =opts? {...buitinOption,...opts}:buitinOption
-    let {name,org,ns}=option
-    let dataInNs=data[ns]
-    let res:string=dataInNs?dataInNs:''
-    data[ns]=genPkgName(option)
+export function editName(data: any, opts?: EditNameOptionLike) {
+    let buitinOption: EditNameOption = builtinEditNameOption
+    let option = opts ? { ...buitinOption, ...opts } : buitinOption
+    let { name, org, ns } = option
+    let dataInNs = data[ns]
+    let res: string = dataInNs ? dataInNs : ''
+    data[ns] = genPkgName(option)
     return res
 }
 
-function genPkgName(data:EditNameOptionLike){
-    let {name,org}=data
-    return org?`@${org}/${name}`:name
+function genPkgName(data: EditNameOptionLike) {
+    let { name, org } = data
+    return org ? `@${org}/${name}` : name
 }
 
 
 // editjson-transform-github
 export interface EditRepoOption {
-    user:string
-    repo:string,
-    name?:string,
-    packageLoc?:string,
-    branch?:string,
-    mono?:boolean
+    user: string
+    repo: string,
+    name?: string,
+    packageLoc?: string,
+    branch?: string,
+    mono?: boolean
 }
 export type EditRepoOptionLike = Partial<EditRepoOption>
-const builtinEditRepoOption= {user:'',repo:'',name:'',mono:false,branch:'main',packageLoc:'packages'} 
-export function editRepo(data:any,opts?:EditRepoOptionLike){
-    let buitinOption:EditRepoOption = builtinEditRepoOption
-    let option =opts? {...buitinOption,...opts}:buitinOption
-    let {user,repo,name,mono}=option
-    if(mono){
+const builtinEditRepoOption = { user: '', repo: '', name: '', mono: false, branch: 'main', packageLoc: 'packages' }
+export function editRepo(data: any, opts?: EditRepoOptionLike) {
+    let buitinOption: EditRepoOption = builtinEditRepoOption
+    let option = opts ? { ...buitinOption, ...opts } : buitinOption
+    let { user, repo, name, mono } = option
+    if (mono) {
 
     }
-    let text = ['https://github.com',user,repo].join('/')
+    let text = ['https://github.com', user, repo].join('/')
 
-    data['repository']= {
+    data['repository'] = {
         "type": "git",
         "url": `git+${text}.git`
-      },
-    data['bugs']= {
-        "url": `${text}/issues`
     },
-    data['homepage'] = mono?`${text}/${getMonoHomePageSuffix(option)}#readme`:`${text}#readme`
-   
+        data['bugs'] = {
+            "url": `${text}/issues`
+        },
+        data['homepage'] = mono ? `${text}/${getMonoHomePageSuffix(option)}#readme` : `${text}#readme`
+
 }
-function getMonoHomePageSuffix(data:EditRepoOptionLike){
-    let {branch,packageLoc,name}=data
-    return ['blob',branch,packageLoc,name].join("/")
+function getMonoHomePageSuffix(data: EditRepoOptionLike) {
+    let { branch, packageLoc, name } = data
+    return ['blob', branch, packageLoc, name].join("/")
 }
 
-type RootJsonData = Record<string,any>
+type RootJsonData = Record<string, any>
 
 /**
  * 
@@ -184,16 +186,16 @@ type RootJsonData = Record<string,any>
  * setJsonValueInNs('a.b.c.d','array',{},'.') //{ a: { b: { c: 'array' } } }
  * ``` 
  */
-export function setJsonValueInNs(key:string,value:any,json:RootJsonData,sep:string=''){
-    let {lastns,context} = getJsonContextInNs(key,json,sep)
-    
-    if(value===undefined){
+export function setJsonValueInNs(key: string, value: any, json: RootJsonData, sep: string = '') {
+    let { lastns, context } = getJsonContextInNs(key, json, sep)
+
+    if (value === undefined) {
         // del ns when set it as undefined
-        delete context[lastns] 
-    }else{
+        delete context[lastns]
+    } else {
         context[lastns] = value
     }
-    
+
     return json
 }
 
@@ -204,22 +206,22 @@ export function setJsonValueInNs(key:string,value:any,json:RootJsonData,sep:stri
  * iniJsonValueInNs('a.b.c.d','array',{},'.') //{ a: { b: { c: [Object] } } }
  * ``` 
  */
-export function iniJsonValueInNs(key:string,type:string,json:RootJsonData,sep:string=''){
-    let {lastns,context} = getJsonContextInNs(key,json,sep)
-    let noop = ()=>{}
+export function iniJsonValueInNs(key: string, type: string, json: RootJsonData, sep: string = '') {
+    let { lastns, context } = getJsonContextInNs(key, json, sep)
+    let noop = () => { }
 
     // no need initing when exist in it
-    if(lastns in context){
+    if (lastns in context) {
         // do nothing
         noop()
-    }else{
+    } else {
         context[lastns] = iniValueByType(type)
     }
     return json
 }
 
-export function iniValueByType(type:string){
-    let res:any
+export function iniValueByType(type: string) {
+    let res: any
     switch (type.trim().toLowerCase()) {
         case 'hash':
             res = {}
@@ -229,7 +231,7 @@ export function iniValueByType(type:string){
             break;
         case 'bool':
             res = false
-                break;
+            break;
         case 'number':
             res = 0
             break;
@@ -243,25 +245,25 @@ export function iniValueByType(type:string){
 // '{},[],false,0,""'.split(",").map(v=>JSON.parse(v)).map(v=>console.log(v))
 
 
-export function getJsonContextInNs(key:string,json:RootJsonData,sep:string=''){
-    let ns = sep?key.split(sep):[key]
-    let lastns = ns[ns.length-1];
-    let ctx:any=json;
-    if(ns.length>1) {
-        for (let index = 0; index < ns.length-1; index++) {
+export function getJsonContextInNs(key: string, json: RootJsonData, sep: string = '') {
+    let ns = sep ? key.split(sep) : [key]
+    let lastns = ns[ns.length - 1];
+    let ctx: any = json;
+    if (ns.length > 1) {
+        for (let index = 0; index < ns.length - 1; index++) {
             const ki = ns[index];
             let ctxi = ctx[ki]
-    
+
             // update context ctx    
-           if(ctxi){
-                ctx=ctxi
-           }else{
-            ctx[ki]={};ctx=ctx[ki]
-           }
+            if (ctxi) {
+                ctx = ctxi
+            } else {
+                ctx[ki] = {}; ctx = ctx[ki]
+            }
             // ctx=ctxi?ctxi:(ctx[ki]={})
         }
     }
-    return {context:ctx,lastns}
+    return { context: ctx, lastns }
 }
 
 // console.log(iniJsonValueInNs('a.b.c.d','array',{},'.'))
