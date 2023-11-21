13dce761b0074e5ded10ed685585ee732d2c89a7
yemiancheng <ymc-github@gmail.com>
2023-11-21 08:16:00 +0800

    feat(core): globy them when pass data

--- src/globy.ts
+++ src/globy.ts
@@ -1,5 +1,5 @@
-import {readdirSync} from "fs"
-import {dirname} from "path"
+import { readdirSync } from "fs"
+import { dirname } from "path"
 
 /**
  * file-list-globy
@@ -8,22 +8,26 @@ import {dirname} from "path"
  * globy(`./tmp/setting*`).map(location=>rmSync(location))
  * ```
  */
-export function globy(exp:string,data?:string[]){
+export function globy(exp: string, data?: string[]) {
+    // feat(core): globy them when pass data
+    if (data) {
+        return data.filter(one => isOneOfThem(one, exp))
+    }
     // let exp = `./tmp/setting*`
-    let dir:string =''
-    let list:string[]=[]
+    let dir: string = ''
+    let list: string[] = []
 
     // ./tmp/setting* -> ./tmp/setting
-    dir=dirname(exp.replace(/\*/,''))
+    dir = dirname(exp.replace(/\*/, ''))
 
     // get file list in dir or use data from the 2th arg
-    list=data?data:readdirSync(dir)
+    list = readdirSync(dir)
     // log(list)
 
     // ./tmp/setting* ->  setting*
-    let filename = exp.replace(/.*\//,'')
+    let filename = exp.replace(/.*\//, '')
     // setting* -> setting.*
-    filename=filename.replace(/\*/,'.*')
+    filename = filename.replace(/\*/, '.*')
     // log(filename)
 
 
@@ -31,14 +35,45 @@ export function globy(exp:string,data?:string[]){
     let reg = new RegExp(filename)
     // log(reg)
 
-    list=list.filter(name=>reg.test(name))
+    list = list.filter(name => reg.test(name))
     // log(list)
 
     // ensure not starts with './'
     // './a/' or 'a/' -> 'a/'
-    let stdedDir = dir.replace(/^\.\//,'')
-    
-    list=list.map(v=>`${dir}/${v}`)
-    
+    let stdedDir = dir.replace(/^\.\//, '')
+
+    list = list.map(v => `${dir}/${v}`)
+
     return list
+}
+
+
+/**
+ * 
+ * @sample
+ * ```
+ * isOneOfThem('src/xx','*commitlog filter*')
+ * ```
+ */
+function isOneOfThem(one: string, them: string | RegExp[]) {
+    // if (!them) return false
+    // let isReg = them.indexOf('*') >= 0
+    // let input: string[] = them.split(",").map(v => v.trim()).filter(v => v)
+    // if (isReg) {
+    //     // feat: regard * as .*
+    //     let inputReg = input.map(v => v.replace(/\*/g, '.*')).map(v => new RegExp(`^${v}`))
+    //     // list = list.filter(vl => !inputReg.some(reg => reg.test(vl)))
+    //     return inputReg.some(reg => reg.test(one))
+    // }
+    // return input.includes(them)
+    let input = Array.isArray(them) ? them : themRegify(them)
+    return input.some(reg => reg.test(one))
+
+}
+function themArrayify(them: string | string[]) {
+    return Array.isArray(them) ? them : them.split(",").map(v => v.trim()).filter(v => v)
+}
+function themRegify(them: string | string[]) {
+    let input = themArrayify(them)
+    return input.map(v => v.replace(/\*/g, '.*')).map(v => new RegExp(`^${v}`))
 }
