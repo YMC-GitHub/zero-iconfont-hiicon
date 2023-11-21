5c10ee384a905389f85fbe0bb1be93327de56700
yemiancheng <ymc-github@gmail.com>
2023-11-21 08:30:00 +0800

    style(core): auto format when save

--- vite.config.ts
+++ vite.config.ts
@@ -3,46 +3,52 @@ import path from 'node:path'
 import { spawn } from 'node:child_process'
 import { builtinModules } from 'node:module'
 import { defineConfig } from 'vite'
+// import plainText from 'vite-plugin-plain-text';
+
 import pkg from './package.json'
 
 const isdev = process.argv.slice(2).includes('--watch')
-const jsfilesDir:string = "lib"
-const typesDir:string = "types"
+const jsfilesDir: string = "lib"
+const typesDir: string = "types"
 
 export default defineConfig({
-  build: {
-    minify: false,
-    outDir:jsfilesDir,
-    emptyOutDir: !isdev,
-    target: 'node14',
-    lib: {
-      entry: 'src/index.ts',
-      formats: ['cjs', 'es'],
-      fileName: format => format === 'es' ? '[name].mjs' : '[name].js',
+    build: {
+        minify: false,
+        outDir: jsfilesDir,
+        emptyOutDir: !isdev,
+        target: 'node14',
+        lib: {
+            entry: 'src/index.ts',
+            formats: ['cjs', 'es'],
+            fileName: format => format === 'es' ? '[name].mjs' : '[name].js',
+        },
+        rollupOptions: {
+            external: [
+                'vite',
+                ...builtinModules,
+                ...builtinModules.map(m => `node:${m}`),
+                ...Object.keys('dependencies' in pkg ? pkg.dependencies as object : {}),
+            ],
+            output: {
+                exports: 'named',
+            },
+        },
     },
-    rollupOptions: {
-      external: [
-        'vite',
-        ...builtinModules,
-        ...builtinModules.map(m => `node:${m}`),
-        ...Object.keys('dependencies' in pkg ? pkg.dependencies as object : {}),
-      ],
-      output: {
-        exports: 'named',
-      },
-    },
-  },
-  plugins: [{
-    name: 'generate-types',
-    async closeBundle() {
-      if (process.env.NODE_ENV === 'test') return
+    plugins: [
+        // allow all *.md files can be import as es module
+        // plainText(['*.md'], { namedExport: false, dtsAutoGen: true, distAutoClean: true },),
+        {
+            name: 'generate-types',
+            async closeBundle() {
+                if (process.env.NODE_ENV === 'test') return
 
-      removeTypes()
-      await generateTypes()
-      moveTypesToDist()
-      removeTypes()
-    },
-  }],
+                removeTypes()
+                await generateTypes()
+                moveTypesToDist()
+                removeTypes()
+            },
+        },
+    ],
 })
 
 function removeTypes() {
@@ -51,26 +57,26 @@ function removeTypes() {
 }
 
 function generateTypes() {
-  return new Promise(resolve => {
-    const cp = spawn(
-      process.platform === 'win32' ? 'npm.cmd' : 'npm',
-      ['run', 'types'],
-      { stdio: 'inherit' },
-    )
-    cp.on('exit', code => {
-      !code && console.log('[types]', 'declaration generated')
-      resolve(code)
+    return new Promise(resolve => {
+        const cp = spawn(
+            process.platform === 'win32' ? 'npm.cmd' : 'npm',
+            ['run', 'types'],
+            { stdio: 'inherit' },
+        )
+        cp.on('exit', code => {
+            !code && console.log('[types]', 'declaration generated')
+            resolve(code)
+        })
+        cp.on('error', process.exit)
     })
-    cp.on('error', process.exit)
-  })
 }
 
 function moveTypesToDist() {
-  const types = path.join(__dirname, typesDir)
-  const dist = path.join(__dirname, jsfilesDir)
-  const files = fs.readdirSync(types).filter(n => n.endsWith('.d.ts'))
-  for (const file of files) {
-    fs.copyFileSync(path.join(types, file), path.join(dist, file))
-    console.log('[types]', `${typesDir}/${file} -> ${jsfilesDir}/${file}`)
-  }
+    const types = path.join(__dirname, typesDir)
+    const dist = path.join(__dirname, jsfilesDir)
+    const files = fs.readdirSync(types).filter(n => n.endsWith('.d.ts'))
+    for (const file of files) {
+        fs.copyFileSync(path.join(types, file), path.join(dist, file))
+        console.log('[types]', `${typesDir}/${file} -> ${jsfilesDir}/${file}`)
+    }
 }
