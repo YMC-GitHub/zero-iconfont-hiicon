5e71b56829c99401cb82b1cdbfa3e9394f027436
yemiancheng <ymc-github@gmail.com>
2023-11-21 08:22:00 +0800

    style(core): auto format when save

--- src/touch.ts
+++ src/touch.ts
@@ -1,25 +1,25 @@
 // touch -- mock bash cmd touch
 
-import { readFileSync, writeFileSync, readdirSync, existsSync, statSync,mkdirSync } from 'fs'
-import { dirname,resolve,join} from 'path'
-const {log}=console
+import { readFileSync, writeFileSync, readdirSync, existsSync, statSync, mkdirSync } from 'fs'
+import { dirname, resolve, join } from 'path'
+const { log } = console
 
-export function saveTextFile(loc:string,text=''){
+export function saveTextFile(loc: string, text = '') {
     makedirs(loc)
     log(`[info] out: ${loc}`)
-    writeFileSync(loc,text)
+    writeFileSync(loc, text)
 }
-export function makedirs(loc:string){
+export function makedirs(loc: string) {
     let dir = dirname(resolve(loc))
-    if(!existsSync(dir)) mkdirSync(dir,{recursive:true})
+    if (!existsSync(dir)) mkdirSync(dir, { recursive: true })
 }
 
-export function touch(loc:string){
+export function touch(loc: string, text: string = '') {
     // do not anything when exist loc
-    if(existsSync(loc)) return false
+    if (existsSync(loc)) return false
 
     makedirs(loc)
     // add an empty text file
-    saveTextFile(loc)
+    saveTextFile(loc, text)
     return true
 }
